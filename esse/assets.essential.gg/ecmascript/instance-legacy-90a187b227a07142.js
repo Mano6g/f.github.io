a6d34c4037515cb9f127350bb3c2c380a57d3ae4
! 6g <106712060+Mano6g@users.noreply.github.com>
2024-10-13 20:07:10 -0300

    Add files via upload

---
+++ esse/assets.essential.gg/ecmascript/instance-legacy-90a187b227a07142.js
@@ -0,0 +1,24 @@
+/**
+ * essential-gg / website
+ *
+ * Copyright © 2013-2024 Panascais ehf. and / or subsidiaries. All rights reserved.
+ *
+ * All information contained herein is, and remains the property
+ * of Panascais ehf. and its suppliers, if any.
+ * The intellectual and technical concepts contained herein are
+ * proprietary to Panascais ehf. and its suppliers and may
+ * be covered by European, U.S. and foreign patents, patents in process,
+ * and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Panascais ehf.
+ *
+ * License:
+ *
+ * You are not allowed to copy, modify, merge, publish, distribute,
+ * sublicense, and / or sell copies of the software.
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the software.
+ */
+System.register(["./framework-legacy-9b1bfef5a0d2b748.js"],(function(e,n){"use strict";var r,l;return{setters:[function(e){r=e.E,l=e.A}],execute:function(){var n,a,o,t,u,i=e("b","PRODUCTION".toUpperCase()),c=(e("e",r[i]),JSON.parse(null!==(n=null===(a=document.querySelector("#configuration"))||void 0===a?void 0:a.innerHTML)&&void 0!==n?n:"{}"));e("h","7a666720d09d"),e("l",c.language||"en"),c.warning;var d=c.namespace,f=c.mode;e("n",d),null===(o=document.querySelector('link[rel="canonical"]'))||void 0===o||o.href,null===(t=document.querySelector('link[hreflang]:not([hreflang="x-default"])'))||void 0===t||t.href,null===(u=document.querySelector('link[hreflang="x-default"]'))||void 0===u||u.href,e("a",new l({namespace:d,applicationMode:f}))}}}));
